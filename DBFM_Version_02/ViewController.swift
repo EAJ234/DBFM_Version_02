22ee3b77f0d6179a90864309c36979d05c062082
EAJ234 <Edward>
2014-09-11 00:10:32 +0800

    Initial Commit

---
+++ DBFM_Version_02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DBFM_Version_02
+//
+//  Created by Edward on 14-9-11.
+//  Copyright (c) 2014年 Edward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
